9555cb8f1046d0cfc07a27bc43a4bfb8b55f7355
jitendra kumar <jitendra.kumar@kapylon.com>
2024-12-25 08:37:01 +0530

    create login page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,7 +1,47 @@
-const Login = () =>{
-    return (
-        <h3>Login</h3>
-    )
-}
+import React, { useState } from "react";
+const Login = () => {
+  const [mobie, setMobile] = useState();
+  const [showMobileError, setShowMobileError] = useState(false);
+  const onChangerHandler = (e) => {
+    setMobile(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!mobie) {
+      setShowMobileError(true);
+      return;
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <h1>Welcome to Sequence</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="mobile">Mobile</label>
+          <input
+            type="text"
+            name="mobie"
+            placeholder="+91 Enter Mobile"
+            value={mobie}
+            onChange={(e) => onChangerHandler(e)}
+          />
+        </div>
+        {showMobileError && !mobie ? (
+          <p className="eror">Please enter your mobile</p>
+        ) : null}
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Enter Password" />
+        </div>
+        <p className="eror">Please enter your Password</p>
+        <button type="button" onClick={() => handleSubmit()}>
+          Login
+        </button>
+        <p className="forgot-password">Forgot Password</p>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
